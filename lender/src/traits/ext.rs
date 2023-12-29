39653061d40e9a72ed30f65c3be0f842a3299dc6
Sebastiano Vigna <sebastiano.vigna@gmail.com>
2023-12-30 00:51:17 +0100

    Fixed method name

--- lender/src/traits/ext.rs
+++ lender/src/traits/ext.rs
@@ -1,16 +1,16 @@
 use crate::{FromIntoIter, FromIter};
 
-/// Extension trait adding to [`Iterator`] the method [`into_lend_iter`](IteratorExt::into_lend_iter),
+/// Extension trait adding to [`Iterator`] the method [`into_lender`](IteratorExt::into_lender),
 /// which turns an [`Iterator`] into a [`Lender`](crate::Lender) without allocation.
 pub trait IteratorExt<I: Iterator + Sized> {
     /// Turn this [`Iterator`] into a [`Lender`](crate::Lender) without allocation.
     ///
     /// This method is a convenient entry point for [`from_iter`](crate::from_iter).
-    fn into_lend_iter(self) -> FromIter<I>;
+    fn into_lender(self) -> FromIter<I>;
 }
 
 impl<I: Iterator> IteratorExt<I> for I {
-    fn into_lend_iter(self) -> FromIter<I> {
+    fn into_lender(self) -> FromIter<I> {
         crate::from_iter(self)
     }
 }
