e14c97323fd5b32a00bd5cb9328c779c9c2b22b1
Sebastiano Vigna <sebastiano.vigna@gmail.com>
2023-12-29 12:09:32 +0100

    README

---
+++ lender-derive/README.md
@@ -0,0 +1,15 @@
+Derive procedural macros for the [`lender`](https://crates.io/crates/Lender) crate.
+
+This crate provides a [`for_!`] function-like macro that can be used to iterate over
+an [`IntoLender`](https://docs.rs/lender/latest/lender/trait.IntoLender.html) with a
+syntax similar to a `for` loop:
+```[ignore]
+for_!(x in into_lender {
+    ...
+});
+```
+The macro expands to a `while let` loop that iterates over a
+[`Lender`](https://docs.rs/lender/latest/lender/trait.Lender.html) obtained from the
+[`IntoLender`](https://docs.rs/lender/latest/lender/trait.IntoLender.html).
+The full `for` syntax is supported (patterns, etc.).
+1
--- lender-derive/src/lib.rs
+++ lender-derive/src/lib.rs
@@ -5,23 +5,7 @@
  * SPDX-License-Identifier: Apache-2.0 OR LGPL-2.1-or-later OR MIT
  */
 
-/*!
-
-Derive procedural macros for the [`lender`](https://crates.io/crates/Lender) crate.
-
-This crate provides a [for_!] function-like macro that can be used to iterate over
-a [`IntoLender`](https://docs.rs/lender/latest/lender/trait.IntoLender.html) with a
-syntax similar to a `for` loop:
-```[ignore]
-for_!(x in into_lender {
-    ...
-});
-```
-The macro expands to a `while let` loop that iterates over a
-[`Lender`](https://docs.rs/lender/latest/lender/trait.Lender.html) obtained from the
-[`IntoLender`](https://docs.rs/lender/latest/lender/trait.IntoLender.html).
-The full `for` syntax is suppored (patterns, etc.).
-*/
+#![doc = include_str!("../README.md")]
 
 extern crate proc_macro;
 
