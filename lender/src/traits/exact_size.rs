a94afe245340a851821e2f42181f586774e235d9
WanderLanz <wanderlanz@proton.me>
2024-02-17 00:56:06 -0600

    pinning, generalize peekable with HasNext

--- lender/src/traits/exact_size.rs
+++ lender/src/traits/exact_size.rs
@@ -9,15 +9,31 @@ pub trait ExactSizeLender: Lender {
         lower
     }
     #[inline]
-    fn is_empty(&self) -> bool {
-        self.len() == 0
-    }
+    fn is_empty(&self) -> bool { self.len() == 0 }
 }
-impl<I: ExactSizeLender> ExactSizeLender for &mut I {
-    fn len(&self) -> usize {
-        (**self).len()
-    }
-    fn is_empty(&self) -> bool {
-        (**self).is_empty()
-    }
+impl<L: ExactSizeLender + ?Sized> ExactSizeLender for &mut L {
+    fn len(&self) -> usize { (**self).len() }
+    fn is_empty(&self) -> bool { (**self).is_empty() }
+}
+
+/// A lender that knows if it has additional elements.
+///
+/// Many [`Iterator`]s don't have a use for this kind of trait, but [`Lender`]s do because of lending requirements.
+///
+/// The [`has_next`](HasNext::has_next) method has a default implementation relying on [`size_hint`](Lender::size_hint), so you should
+/// override the implementation if [`size_hint`](Lender::size_hint) is not accurate.
+///
+/// Note that this trait is a safe trait and as such does *not* and *cannot*
+/// guarantee that the returned assertion is correct. This means that `unsafe`
+/// code **must not** rely on the correctness of [`has_next`](HasNext::has_next).
+pub trait HasNext: Lender {
+    /// Returns true if there are more lends, and false otherwise.
+    fn has_next(&mut self) -> bool { self.size_hint().0 > 0 }
+}
+
+impl<L: ?Sized> HasNext for L
+where
+    L: ExactSizeLender,
+{
+    fn has_next(&mut self) -> bool { !self.is_empty() }
 }
--- lender/src/traits/mod.rs
+++ lender/src/traits/mod.rs
@@ -10,7 +10,7 @@ pub use self::{
     accum::{ProductLender, SumLender},
     collect::{ExtendLender, FromLender, IntoLender},
     double_ended::DoubleEndedLender,
-    exact_size::ExactSizeLender,
+    exact_size::{ExactSizeLender, HasNext},
     ext::{IntoIteratorExt, IteratorExt},
     lender::{Lend, Lender, Lending},
     marker::FusedLender,
@@ -26,25 +26,19 @@ impl<'a, A: 'a, B: 'a> TupleLend<'a> for (A, B) {
     type First = A;
     type Second = B;
     #[inline(always)]
-    fn tuple_lend(self) -> (Self::First, Self::Second) {
-        (self.0, self.1)
-    }
+    fn tuple_lend(self) -> (Self::First, Self::Second) { (self.0, self.1) }
 }
 impl<'a, A, B> TupleLend<'a> for &'a (A, B) {
     type First = &'a A;
     type Second = &'a B;
     #[inline(always)]
-    fn tuple_lend(self) -> (Self::First, Self::Second) {
-        (&self.0, &self.1)
-    }
+    fn tuple_lend(self) -> (Self::First, Self::Second) { (&self.0, &self.1) }
 }
 impl<'a, A, B> TupleLend<'a> for &'a mut (A, B) {
     type First = &'a mut A;
     type Second = &'a mut B;
     #[inline(always)]
-    fn tuple_lend(self) -> (Self::First, Self::Second) {
-        (&mut self.0, &mut self.1)
-    }
+    fn tuple_lend(self) -> (Self::First, Self::Second) { (&mut self.0, &mut self.1) }
 }
 
 /// Internal struct used to implement [`lend!`], do not use directly.
