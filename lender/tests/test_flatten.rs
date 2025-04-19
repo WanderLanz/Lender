30258347d1cf9ae44c90a89c8e96912d60cb8fab
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2025-04-19 07:19:04 +0200

    Further simplified test

--- lender/tests/test_flatten.rs
+++ lender/tests/test_flatten.rs
@@ -1,52 +1,30 @@
 use lender::prelude::{Lender, Lending};
 
 struct Child<'lend> {
-    data: &'lend mut Data,
-    found_circuit: bool,
+    stack: &'lend mut Vec<usize>,
+    returned: bool,
 }
 
 impl<'lend> From<&'lend mut InnerParent> for Child<'lend> {
     fn from(parent: &'lend mut InnerParent) -> Self {
-        let mut circuit_search = Self {
-            data: &mut parent.data,
-            found_circuit: false,
-        };
-        circuit_search.push_to_stack(0);
-        circuit_search
+        let mut child = Self { stack: &mut parent.stack, returned: false };
+        child.push_to_stack(0);
+        child
     }
 }
 
 impl<'lend> Child<'lend> {
     fn push_to_stack(&mut self, row_id: usize) {
-        println!(
-            "Pushing row {row_id} to stack at address {}",
-            self.data.stack.as_ptr() as usize
-        );
-        self.data.stack.push(row_id);
-        println!(
-            "Stack at address {} after push: {:?}",
-            self.data.stack.as_ptr() as usize,
-            self.data.stack
-        );
+        println!("Pushing row {row_id} to stack at address {}", self.stack.as_ptr() as usize);
+        self.stack.push(row_id);
+        println!("Stack at address {} after push: {:?}", self.stack.as_ptr() as usize, self.stack);
     }
 
     fn pop_from_stack(&mut self) {
-        println!(
-            "Popping row {} from stack at address {}",
-            self.data.stack.last().unwrap(),
-            self.data.stack.as_ptr() as usize
-        );
-        println!(
-            "Stack at address {} before pop: {:?}",
-            self.data.stack.as_ptr() as usize,
-            self.data.stack
-        );
-        self.data.stack.pop().unwrap();
-        println!(
-            "Stack at address {} after pop: {:?}",
-            self.data.stack.as_ptr() as usize,
-            self.data.stack
-        );
+        println!("Popping row {} from stack at address {}", self.stack.last().unwrap(), self.stack.as_ptr() as usize);
+        println!("Stack at address {} before pop: {:?}", self.stack.as_ptr() as usize, self.stack);
+        self.stack.pop().unwrap();
+        println!("Stack at address {} after pop: {:?}", self.stack.as_ptr() as usize, self.stack);
     }
 }
 
@@ -56,25 +34,19 @@ impl<'lend2, 'lend> Lending<'lend2> for Child<'lend> {
 
 impl<'lend> Lender for Child<'lend> {
     fn next<'lend2>(&'lend2 mut self) -> Option<<Self as Lending<'lend2>>::Lend> {
-        if self.found_circuit {
+        if self.returned {
             self.pop_from_stack();
             None
         } else {
-            self.found_circuit = true;
-            Some(&self.data.stack)
+            self.returned = true;
+            Some(&self.stack)
         }
     }
 }
 
-struct Data {
-    /// The stack of nodes in the current circuit.
-    stack: Vec<usize>,
-}
-
-/// Iterator for Johnson's algorithm.
 struct InnerParent {
     /// The underlying data structure for the algorithm.
-    data: Data,
+    stack: Vec<usize>,
     current_root_id: usize,
 }
 
@@ -85,11 +57,11 @@ impl<'lend> Lending<'lend> for InnerParent {
 impl Lender for InnerParent {
     fn next<'lend>(&'lend mut self) -> Option<<Self as Lending<'lend>>::Lend> {
         while self.current_root_id < 5 {
-            assert!(
-                self.data.stack.is_empty(),
-                "Stack at address {} should be empty at the start of the circuit search, but in parent is {:?}",
-                self.data.stack.as_ptr() as usize,
-                self.data.stack
+            debug_assert!(
+                self.stack.is_empty(),
+                "Stack at address {} should be empty at the start of the inner child loop, but in parent is {:?}",
+                self.stack.as_ptr() as usize,
+                self.stack
             );
 
             self.current_root_id += 1;
@@ -100,29 +72,20 @@ impl Lender for InnerParent {
     }
 }
 
-/// Johnson's algorithm for finding all cycles in a sparse matrix.
 pub struct Parent<'parent> {
     /// The underlying iterator.
     inner: lender::Flatten<'parent, InnerParent>,
 }
 
-impl<'matrix> Iterator for Parent<'matrix> {
+impl<'parent> Iterator for Parent<'parent> {
     type Item = Vec<usize>;
 
-    fn next(&mut self) -> Option<Self::Item> {
-        self.inner.next().map(|circuit| circuit.to_vec())
-    }
+    fn next(&mut self) -> Option<Self::Item> { self.inner.next().map(|vec| vec.to_vec()) }
 }
-
 
 #[test]
 fn test_flatten() {
-	let inner_parent = InnerParent {
-        data: Data { stack: Vec::new() },
-        current_root_id: 0,
-    };
-    let parent = Parent {
-        inner: inner_parent.flatten(),
-    };
+    let inner_parent = InnerParent { stack: Vec::new(), current_root_id: 0 };
+    let parent = Parent { inner: inner_parent.flatten() };
     let results = parent.collect::<Vec<_>>();
 }
